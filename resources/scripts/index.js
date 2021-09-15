7da654d64dc2b80744f43db79cd6cb7650425f25
Falconsc <75039653+Falcons1231@users.noreply.github.com>
2021-09-15 16:43:48 -0400

    updated rock paper scissors.js

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,12 +1,12 @@
 // Elements
 const welcomeScreen = document.getElementById(`welcome-screen`);
-const gameScreen = ;
-const startGameButton = ;
-const userName = ;
-const userSelection = ;
-const goButton = ;
-const scoreParagraph = ;
-const gameHistoryParagraph = ;
+const gameScreen = document.getElementById("game-screen");
+const startGameButton = document.getElementById("start-game-button");
+const userName = document.getElementById("username");
+const userSelection = document.getElementById("user-selection");
+const goButton = document.getElementById("go-button");
+const scoreParagraph = document.getElementById("score");
+const gameHistoryParagraph = document.getElementById("game-history");
 
 // instantiate the game object from the `RockPaperScissors` class.
 let game;
@@ -16,24 +16,31 @@ gameScreen.classList.add(`d-none`);
 
 // updateScoreTallyUI
 function updateScoreTallyUI(){
-
+  scoreParagraph.innerHTML = game.userName + ": " + game.user.score + " v CPU: " + game.cpu.score;
 }
 
 // updateGameHistoryUI
 function updateGameHistoryUI(){
-
+  gameHistoryParagraph.innerHTML = game.gameHistoryLog;
 }
 
 // start-game-button EventListener
-startGameButton.addEventListener(`click`, function () {
-  const username = 
+startGameButton.addEventListener(`click`, function (startButtonClickEvent) {
+  startButtonClickEvent.preventDefault();
+  const username = userName;
   game = new RockPaperScissors(userName);
-  // Complete
+  welcomeScreen.classList.add('d-none');
+  gameScreen.classList.remove('d-none');
+    // Complete
 });
 
 // go-button EventListener
-goButton.addEventListener(`click`, function () {
-  
+goButton.addEventListener(`click`, function (goButtonClickEvent) {
+  goButtonClickEvent.preventDefault();
+  game.play(userSelection);
+  updateScoreTallyUI();
+  gameHistoryParagraph();
+
 });
 
 // If you're doing the extra-credit, uncomment the below: reset-game-button
